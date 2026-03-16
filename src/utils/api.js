b65cfcb62f04371ed6ea883da7f61e4c97f535da
German <germandase@gmail.com>
2026-03-16 11:02:48 -0500

    agregar permisos

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://api-sigesott.duckdns.org:8000',  // Asegúrate de que esta URL sea correcta
+  baseURL: 'https://api-sigesott.duckdns.org:8000',  // Asegúrate de que esta URL sea correcta
 
 });
 
