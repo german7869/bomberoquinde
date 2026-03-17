1b3fdf8ff65c4dcc4c1bb79f0e4a05ad847bb6b6
German <germandase@gmail.com>
2026-03-17 09:38:55 -0500

    http duck usan

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://dizziest-kaia-unsantirically.ngrok-free.dev',  // Asegúrate de que esta URL sea correcta
+  baseURL: 'https://dizziest-kaia-unsatirically.ngrok-free.dev',  // Asegúrate de que esta URL sea correcta
                  
 });
 
