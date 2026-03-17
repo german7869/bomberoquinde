160e245260f00b1c33a9c0a816d98d807e3619c6
German <germandase@gmail.com>
2026-03-17 09:33:28 -0500

    https://api-sigesott.duckdns.org:8000

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://dizziest-kaia-unsantirically.ngrok-free.dev',  // Asegúrate de que esta URL sea correcta
-
+  baseURL: 'dizziest-kaia-unsantirically.ngrok-free.dev',  // Asegúrate de que esta URL sea correcta
+                 
 });
 
 export default api;
